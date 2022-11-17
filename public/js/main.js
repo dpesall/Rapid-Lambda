5bfd54cd5aad2c4a61e82b743c29f28b3b1964e4
Drew Pesall <pesallandrew@gmail.com>
2022-11-17 11:05:10 -0600

    Year time variables

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 const DAY_MINUTES = 1440;
 const DAY_SECONDS = 86400;
 const DAY_MILLISECONDS = DAY_SECONDS * 1000;
+const YEAR_MINUTES = DAY_MINUTES * 365;
+const YEAR_SECONDS = DAY_SECONDS * 365;
+const YEAR_MILLISECONDS = DAY_MILLISECONDS * 365;
 
 function log(msg) {
     console.log(msg);
